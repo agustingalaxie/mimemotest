559e7ed29a6dc05e0ffa7f2d3acfd3bba1617ef1
agustingalaxie <71106735+agustingalaxie@users.noreply.github.com>
2021-05-10 21:49:16 -0300

    mostrar contenido del cuadro
    empce el codigo para mostrar el contenido cuando se clickea

---
+++ main.js
@@ -0,0 +1,22 @@
+const cuadros = document.querySelectorAll("cuadro");
+
+cuadros.onclick = function (){
+
+}
+const cuadro = document.getElementById("asd");
+cuadro.onclick = function (e){
+    e.target.className = "col-3 cuadro clickeado";
+    e.preventDefault;
+}
+//function onclick de inicio
+    //function que atribuye valores a los cuadros, dos de cada valor
+    
+    //que el cuadro se abra desde el medio
+    
+
+//funcion onclick cuadro
+// "dar vuelta" el cuadro y mostrar un valor
+// "dar vuelta" otro cuadro y mostrar un valor
+// 1 segundo
+// si se acierta los dos valores juntos los cuadros quedan dados vuelta y cambian de color
+// si no los dos vuelven a esconderse
