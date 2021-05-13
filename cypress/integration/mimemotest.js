3fe4bf549a54e3600f3b1b77b04cb9992414d4e1
agustingalaxie <71106735+agustingalaxie@users.noreply.github.com>
2021-05-13 16:22:49 -0300

    implementacion-cypress
    implemento cypress en una branch aparte por las dudas. la aplicacion ya estaba funcional, y no quiero hacerle grandes cambios, solo practicar su testing.
    implementation cypress
    i implement cypress on a different branch just in case, the app was functional, and i dont want to make big changes on it, just practice its testing

--- cypress/integration/mimemotest.js
+++ cypress/integration/mimemotest.js
@@ -1,12 +1,31 @@
 //const { before } = require("cypress/types/lodash");
 
+//import { it } from "mocha";
+
 const URL = "127.0.0.1:5500";
+const CANTIDAD_CUADROS = 16;
 
 context('mimemotest', () => {
     before(() => {
         cy.visit(URL);
     });
-    it('tests', () => {
-
+    it('Se asegura de que haya un tablero con 16 cuadros', () => {
+        cy.get("#grilla-cuadros").find(".cuadro").should('have.length', CANTIDAD_CUADROS)
+    });
+    it('Se asegura de que los cuadros sean aleatorios', () => {
+        cy.get("button").click().get(".oculto").then(ocultos => {
+            const primerosContenidos = [];
+            ocultos.each(function (i, oculto) {
+                primerosContenidos.push(oculto.innerText)
+            });
+        });
+        cy.visit(URL);
+        cy.get("button").click().get(".oculto").then(ocultos => {
+            const segundosContenidos = [];
+            ocultos.each(function (i, oculto) {
+                segundosContenidos.push(oculto.innerText)
+            });
+            cy.wrap(primerosContenidos).should('not.deep.equal', segundosContenidos)
+        })
     })
 })
