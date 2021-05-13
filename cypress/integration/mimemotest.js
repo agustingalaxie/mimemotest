3d51aa76af0eef6d96d7638a41db8852ecb92b0f
agustingalaxie <71106735+agustingalaxie@users.noreply.github.com>
2021-05-13 18:27:06 -0300

    update con tres tests en cypress
    agrego tres pruebas de funcionamiento mediante cypress, 'Se asegura de que haya un tablero con 16 cuadros', 'Se asegura de que los tableros se escondan al apretar inicio con tableros dados vuelta', 'Se asegura de que los cuadros sean aleatorios'.  El siguiente paso es hacer un test que se equivoque y chequee que los cuadros vuelvan a estar dados vuelta, y otro que resuelva el juego.
    i add three functional tests using cypress, "it checks the existence of a board with 16 squares", "it checks that the squares hide when clicking start with face-up squares", "it checks that the squares are random". The next step is making a test that checks for squares to be hidden in mistakes, and another that finishes the game.

--- cypress/integration/mimemotest.js
+++ cypress/integration/mimemotest.js
@@ -4,6 +4,8 @@
 
 const URL = "127.0.0.1:5500";
 const CANTIDAD_CUADROS = 16;
+const CLASE_CUADROS_DADOS_VUELTA = "col-3 cuadro clickeado";
+const CLASE_CUADROS_ESCONDIDOS = "col-3 cuadro";
 
 context('mimemotest', () => {
     before(() => {
@@ -12,20 +14,34 @@ context('mimemotest', () => {
     it('Se asegura de que haya un tablero con 16 cuadros', () => {
         cy.get("#grilla-cuadros").find(".cuadro").should('have.length', CANTIDAD_CUADROS)
     });
+    it('Se asegura de que los cuadros se escondan al apretar inicio con cuadros dados vuelta', () => {
+        cy.get("button").click();
+        cy.wait(500);
+        cy.get("#cuadro1").click();
+        cy.get("#grilla-cuadros").find("#cuadro1").should('have.class', CLASE_CUADROS_DADOS_VUELTA);
+        cy.get("button").click();
+        cy.get("#grilla-cuadros").find(".cuadro").should('have.class', CLASE_CUADROS_ESCONDIDOS);
+    });
     it('Se asegura de que los cuadros sean aleatorios', () => {
-        cy.get("button").click().get(".oculto").then(ocultos => {
-            const primerosContenidos = [];
+        let primerosContenidos = [];
+        let segundosContenidos = [];
+        cy.get("button").click()
+        cy.wait(500);
+        cy.get(".oculto").then(ocultos => {            
             ocultos.each(function (i, oculto) {
-                primerosContenidos.push(oculto.innerText)
+                primerosContenidos.push(oculto.innerHTML);
             });
         });
         cy.visit(URL);
-        cy.get("button").click().get(".oculto").then(ocultos => {
-            const segundosContenidos = [];
-            ocultos.each(function (i, oculto) {
-                segundosContenidos.push(oculto.innerText)
-            });
-            cy.wrap(primerosContenidos).should('not.deep.equal', segundosContenidos)
+        cy.get("button").click();
+        cy.wait(500);
+        cy.get(".interior").then((interior) => {
+            interior.each(function (i, interior) {
+                segundosContenidos.push(interior.innerText)
+            });            
         })
+        cy.wrap(primerosContenidos).should('not.deep.equal', segundosContenidos);
     })
-})
+
+}
+) 
